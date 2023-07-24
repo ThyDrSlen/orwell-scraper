7e8730aed4ea4f78160de08accf3b73dd9e10bee
Fabrizio Corrales <drslen9@gmail.com>
2023-07-24 18:59:11 -0400

    added status code

--- bot.py
+++ bot.py
@@ -5,64 +5,71 @@ import aiohttp
 import ssl
 import random
 from fake_useragent import UserAgent
+from rich.console import Console
+from tqdm.asyncio import tqdm as async_tqdm
 
+console = Console()
 
-auth = os.environ.get('AUTH')
-browser_url = f'https://{auth}@brd.superproxy.io:9222'
-
-#TODO 
-# setup array so that multiple auths can be used 
-# keep track of time or how far scrolled down 
 ua = UserAgent()
 
 async def download_image(url, filepath):
     if os.path.exists(filepath):
-        print(f'Skipping {url} - already downloaded')
+        console.print(f'📁 Skipping {url} - already downloaded\n', style="bold red")
+        console.print('-' * 50)
         return
     async with aiohttp.ClientSession() as session:
         try:
             async with session.get(url, ssl=ssl.SSLContext()) as resp:  # SSL verification disabled
-                if resp.status == 200:
+                if resp.status >= 200 and resp.status < 300:  # Success
                     with open(filepath, 'wb') as f:
                         f.write(await resp.read())
-                    # await asyncio.sleep(random.uniform(1, 3)) #MAYBE CHANGE THIS
-                    print(f'Downloaded {url}')
+                    console.print(f'✅ Downloaded {url}\n', style="bold green")
                 else:
-                    print(f'Failed to download {url}')
+                    console.print(f'⚠️ Failed to download {url}, status code {resp.status}\n', style="bold red")
+                console.print('-' * 50)
         except Exception as e:
-            print(f'Error downloading {url}: {e}')
+            console.print(f'🚫 Error downloading {url}: {e}\n', style="bold red")
+            console.print('-' * 50)
 
 async def asyncdefmain():
     async with async_playwright() as pw:
-        print('Connecting to browser...')
+        console.print('Connecting to browser...\n', style="bold blue")
         browser = await pw.chromium.launch(headless=False)
-        print('Connected to browser.')
+        console.print('Connected to browser.\n', style="bold green")
+        console.print('-' * 50)
         page = await browser.new_page()
         await page.set_extra_http_headers({'User-Agent': ua.random})
-        print('Navigating to page...')
+        console.print('Navigating to page...\n', style="bold blue")
         await page.goto('https://www.goat.com/search', timeout=160000)
-        print('Page loaded.')
+        console.print('Page loaded.\n', style="bold green")
+        console.print('-' * 50)
         await page.wait_for_selector('.jugzgk, .kjpMyF')
-        print('Evaluating page...')
+        console.print('Evaluating page...\n', style="bold blue")
+        console.print('-' * 50)
 
+        progress_bar = async_tqdm(total=25, ncols=100, desc="Scrolling and Downloading Images")  # tqdm progress bar
         for _ in range(25):  # Scroll 25 times
             image_elements = await page.query_selector_all('.jugzgk, .kjpMyF')
             for image in image_elements:
                 url = await image.get_attribute('srcset')
-                url = await image.get_attribute('srcset')
                 if url is not None:
-                    url = url.split(", ")[7].split(" ")[0]   
+                    url = url.split(", ")[-1].split(" ")[0]   
                 alt_text = await image.get_attribute('alt')
                 class_name = 'shoes' if 'kjpMyF' in (await image.get_attribute('class') or '') else 'clothes'
                 if not os.path.exists(class_name):
                     os.makedirs(class_name)
                 filename = f"{alt_text}.png"
                 filepath = os.path.join(class_name, filename)
-                print(f'Downloading {url} to {filepath}...')
+                console.print(f'⌛ Downloading {url} to {filepath}...\n', style="bold blue")
                 await download_image(url, filepath)
             await page.evaluate(f"window.scrollBy(0, {random.uniform(300, 7000)});")  # Random scroll distance
             await asyncio.sleep(random.uniform(1, 3.5))  # Random delay between scrolls
-        print('Closing browser...')
+            progress_bar.update(1)  # update progress after each scroll
+            progress_bar.refresh()  # manually refresh the progress bar
+        progress_bar.close()
+
+        console.print('Closing browser...\n', style="bold blue")
+        console.print('-' * 50)
         await browser.close()
 
 def main():
